14dfd8d9f54708ad7974bdfa4631a69dc9feb9de
PrinceRamawat1921 <96066829+PrinceRamawat1921@users.noreply.github.com>
2022-11-14 18:03:01 +0530

    Firebase Web Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kshri2/Screens/sign_in_screen.dart';
 import 'package:kshri2/utils/color_themes.dart';
 
-void main() {
+void main() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBrJGeYTFwoG6c5PvCf85EeLrCYk1Hl1I0",
+        authDomain: "kshriapp.firebaseapp.com",
+        projectId: "kshriapp",
+        storageBucket: "kshriapp.appspot.com",
+        messagingSenderId: "364508896914",
+        appId: "1:364508896914:web:b8fbae669c0295163a904b",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const Kshri());
 }
 
---
+++ lib/widgets/custom_main_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:kshri2/utils/utils.dart';
+
+class CustomMainButton extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  final bool isLoading;
+  final VoidCallback onPressed;
+  const CustomMainButton({
+    super.key,
+    required this.child,
+    required this.color,
+    required this.isLoading,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = Utils().getScreenSize();
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: color,
+        fixedSize: Size(
+          screenSize.width * 0.5,
+          40,
+        ),
+      ),
+      onPressed: onPressed,
+      child: !isLoading
+          ? child
+          : const Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+    );
+  }
+}
