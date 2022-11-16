a20e2f866e355e4062fef96e6b109a6cd4536d0c
PrinceRamawat1921 <96066829+PrinceRamawat1921@users.noreply.github.com>
2022-11-16 19:35:34 +0530

    Changed != to isNotEmpty

--- lib/resources/authentication_methods.dart
+++ lib/resources/authentication_methods.dart
@@ -5,12 +5,11 @@ class AuthenticationMethods {
     required String email,
     required String password,
   }) async {
-    name.trim();
-    address.trim();
-    email.trim();
-    password.trim();
     String output = "Something went wrong";
-    if (name != "" && address != "" && email != "" && password != "") {
+    if (name.isNotEmpty &&
+        address.isNotEmpty &&
+        email.isNotEmpty &&
+        password.isNotEmpty) {
       output = "success";
     } else {
       output = "Please fill up everything";
